eb1a513fd697c0d60d0bcc4df54422be386de34d
James Lal <james@lightsofapollo.com>
2015-03-11 23:18:50 -0700

    Initial working prototype of docopt loading/command running

---
+++ src/index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import 'babel/polyfill'
+import yaml from 'js-yaml';
+import fsPath from 'path';
+import fs from 'mz/fs';
+import createCommands from './commands';
+
+const CONFIG = 'exhibition.yml';
+
+async function main(cwd) {
+  // Resolve cwd to an absolute path...
+  cwd = fsPath.resolve(cwd);
+
+  let configPath = fsPath.join(cwd, CONFIG);
+
+  if (!fs.exists(config)) {
+    throw new Error(`
+      Config file is missing (${configPath})
+    `);
+  }
+
+  let config = yaml.safeLoad(await fs.readFile(configPath, 'utf8'));
+  let commands = await createCommands(cwd, config);
+
+  await commands.run(process.argv.slice(3));
+}
+
+main(process.argv[2]).catch((e) => {
+  setTimeout(() => {
+    throw e;
+  });
+});
